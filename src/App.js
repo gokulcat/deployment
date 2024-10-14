bb0e70a01be2d2bf2227145dffd043edbeacb54a
gokulcat <75694391+gokulcat@users.noreply.github.com>
2024-10-14 21:02:07 +0530

    Added Button to Change Theme

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./index.css";
 
 function Counter() {
   const [count, setCount] = useState(0);
+  const [darkMode, setDarkMode] = useState(false);
 
   const increment = () => {
     setCount(count + 1);
@@ -14,15 +15,31 @@ function Counter() {
     setCount(0);
   };
 
+  const toggleTheme = () => {
+    setDarkMode((prevMode) => !prevMode);
+  };
+  const themeStyles = {
+    backgroundColor: darkMode ? "#333" : "#fff",
+    color: darkMode ? "#fff" : "#000",
+    height: "97vh",
+    display: "flex",
+  };
+
   return (
-    <div className="container">
-      <h1>Counter : {count}</h1>
-      <button onClick={increment}> Increment</button>
-      <br />
-      <button onClick={decrement}>Decrement</button>
-      <br />
-      <button onClick={reset}>Reset</button>
-    </div>
+    <>
+      <div className="container" style={themeStyles}>
+        <h1>Counter : {count}</h1>
+        <button onClick={increment}> Increment</button>
+        <br />
+        <button onClick={decrement}>Decrement</button>
+        <br />
+        <button onClick={reset}>Reset</button>
+        <br />
+        <button onClick={toggleTheme}>
+          Switch to {darkMode ? "Light" : "Dark"} Mode
+        </button>
+      </div>
+    </>
   );
 }
 
